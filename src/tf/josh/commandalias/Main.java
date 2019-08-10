4d4e4bde114128d00e6fef5d51df69c22af300cb
Josh Bowden <git@josh.tf>
2019-08-11 02:24:02 +1000

    create jar build config and add default functions

--- src/tf/josh/commandalias/Main.java
+++ src/tf/josh/commandalias/Main.java
@@ -4,4 +4,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
+    @Override
+    public void onEnable() {
+    	
+    }
+    
+    @Override
+    public void onDisable() {
+    }
+	
 }
