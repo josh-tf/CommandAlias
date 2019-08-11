b2410b9de5e1eb8c7e108c9f175707112a1304bb
Josh Bowden <git@josh.tf>
2019-08-11 12:58:56 +1000

    update chat class

--- src/tf/josh/commandalias/cmd/Chat.java
+++ src/tf/josh/commandalias/cmd/Chat.java
@@ -1,5 +1,31 @@
 package tf.josh.commandalias.cmd;
 
-public class Chat {
+import java.util.Arrays;
 
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.defaults.BukkitCommand;
+import org.bukkit.entity.Player;
+
+public class Chat
+        extends BukkitCommand {
+    public Chat() {
+        super("c", "chat", "/c", Arrays.asList(new String[]{""}));
+    }
+
+    public boolean execute(CommandSender sender, String label, String[] args) {
+        if (sender instanceof Player) {
+            StringBuilder builder = new StringBuilder();
+            byte b;
+            int i;
+            String[] arrayOfString;
+            for (i = arrayOfString = args.length, b = 0; b < i; ) {
+                String s = arrayOfString[b];
+                builder.append(String.valueOf(s) + " ");
+                b++;
+            }
+            Player p = (Player) sender;
+            p.chat(builder.toString());
+        }
+        return true;
+    }
 }
